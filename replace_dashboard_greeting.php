6c967800d4349580468f6c94b2dc7d81ef2cc746
kuokoad <55944530+kuokoad@users.noreply.github.com>
2020-08-11 17:08:48 +0000

    Create replace_dashboard_greeting.php

---
+++ replace_dashboard_greeting.php
@@ -0,0 +1,10 @@
+<?php
+function replace_greeting( $wp_admin_bar ) {
+    $my_account=$wp_admin_bar->get_node('my-account');
+    $newtitle = str_replace( 'Howdy,', 'Logged in as', $my_account->title );            
+    $wp_admin_bar->add_node( array(
+        'id' => 'my-account',
+        'title' => $newtitle,
+    ) );
+}
+add_filter( 'admin_bar_menu', 'replace_greeting',25 );
