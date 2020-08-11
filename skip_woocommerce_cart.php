4da83042d6f36dce00858b50776bf2cb4644bcaf
kuokoad <55944530+kuokoad@users.noreply.github.com>
2020-08-11 16:53:46 +0000

    Update skip_woocommerce_cart.php

--- skip_woocommerce_cart.php
+++ skip_woocommerce_cart.php
@@ -1,7 +1,7 @@
 <?php
-add_filter('add_to_cart_redirect', 'cw_redirect_add_to_cart');
 function cw_redirect_add_to_cart() {
     global $woocommerce;
-    $cw_redirect_url_checkout = $woocommerce->cart->get_checkout_url();
+        $cw_redirect_url_checkout = $woocommerce->cart->get_checkout_url();
     return $cw_redirect_url_checkout;
 }
+add_filter('add_to_cart_redirect', 'cw_redirect_add_to_cart');
